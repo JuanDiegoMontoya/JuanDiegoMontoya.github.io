bbd8b1aa4a99090e6e2ac8bc79079f555adb2993
Jake <>
2021-10-16 22:12:22 -0700

    Added generated files.

--- generator.py
+++ generator.py
@@ -1,3 +1,11 @@
+# This script generates html files.
+# It first reads the files "header" and "footer" in its current directory.
+# Next, it recursively traverses the current tree for any file
+# with the extension '.htm' (which indicates an "unfinished" html file
+# for the purposes of this script). Then, the header and footer are
+# pre- and post-pended to the current file's contents. The result
+# is written to a new file in the current directory with the extension '.html'.
+
 import os
 
 rootdir = "."
@@ -19,7 +27,7 @@ for subdir, dirs, files in os.walk(rootdir):
             print(os.path.splitext(base)[0] + ".html")
 
             with open(filepath, "r") as f:
-                print("".join(header + f.readlines() + footer))
-                # tf = open(os.path.splitext(base)[0] + ".html", "w+")
-                # tf.write("".join(header + f.readlines() + footer))
-                # tf.close()
+                #print("".join(header + f.readlines() + footer))
+                tf = open(os.path.splitext(base)[0] + ".html", "w+")
+                tf.write("".join(header + f.readlines() + footer))
+                tf.close()
